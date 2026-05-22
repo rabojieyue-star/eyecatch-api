4fe27737c4517e59fc596d1ddbe3ad4ea3dd5e7c
rabojieyue-star <rabojieyue@gmail.com>
2026-05-23 02:44:22 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-apt-get install -y fonts-noto-cjk
-mkdir -p /app/fonts
-cp /usr/share/fonts/opentype/noto/NotoSerifCJK-Bold.ttc /app/fonts/
-cp /usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc /app/fonts/
 pip install -r requirements.txt
